75de1a725ed3725f65aee40b837c5587c7a8fd64
KKA-0 <karanyobro@gmail.com>
2023-02-03 20:17:56 +0530

    added remote sql

--- templates/conn.php
+++ templates/conn.php
@@ -3,5 +3,5 @@
 ?>
 
 <?php
-$con=mysqli_connect('sql105.epizy.com','epiz_33250279','WD38MJch2PWoiQ','epiz_33250279_quiska');
+$con=mysqli_connect('sql.freedb.tech','freedb_KKA123','5%fkPvyRWp6*HQA','freedb_bolo-coder');
 ?>
