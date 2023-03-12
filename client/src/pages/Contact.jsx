80c0a2821709be1750c2de8469f53ce24d3a1eef
Harshvardhan Singh Jadon <jadonharsh109@gmail.com>
2023-03-12 23:20:45 +0530

    contact css page added

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,51 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import Pic from "../images/contact.jpg"
+import "./Contact.css"
 
 const Contact = () => {
-  const Wrapper = styled.section`
-    padding: 0 0 5rem 0;
-    text-align: center;
-    .container {
-      margin-top: 6rem;
-      .contact-form {
-        max-width: 50rem;
-        margin: auto;
-        .contact-inputs {
-          display: flex;
-          flex-direction: column;
-          gap: 3rem;
-          input[type="submit"] {
-            cursor: pointer;
-            transition: all 0.2s;
-            &:hover {
-              background-color: #fff;
-              border: 1px solid rgb(255, 255, 255);
-              color: red ;
-              transform: scale(0.9);
-            }
-          }
-        }
-      }
-    }
-    .contact-inputs input, textarea{
-      padding: 1rem;
-    }
-    /* &::before {
-      content: "";
-      height: 125vh;
-      z-index: -1;
-      position: absolute;
-      width: 100%;
-      background: url("images/bg2.jpg") no-repeat center center/cover;
-      opacity: 0.85;
-      top: 0px;
-      left: 0px;
-    } */
-  `;
 
   return (
-    <Wrapper>
+    <div className="contact-page-css">
       <div className="container">
         <h2 className="common-heading">Feel Free To Contact Us</h2>
         <div className="grid grid-two-column">
@@ -87,7 +47,7 @@ const Contact = () => {
           </div>
         </div>
       </div>
-    </Wrapper>
+    </div>
   );
 };
 
