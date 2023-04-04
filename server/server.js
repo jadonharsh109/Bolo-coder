04cde8cd6cf4e113fe9649b4c0842afb2dcacb57
KKA-0 <karanyobro@gmail.com>
2023-04-04 12:47:18 +0530

    mongodb connected

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const app = require('./App');
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose');
 
 dotenv.config({path: './config.env'});
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
+
+const DB = process.env.DATABASE.replace(
+    '<DATABASE_PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+}).then(() => console.log("DB Connected🎉..."));
 
 console.log(process.env)
 app.listen(port, () => {
